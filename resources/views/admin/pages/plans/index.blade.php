b8718e4be7c2263423aaff1e3d0926f5f6c21009
Higor23 <higor.lemospr@gmail.com>
2020-08-10 10:23:42 +0000

    Create and Store - Plans

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -3,9 +3,40 @@
 @section('title', 'Planos')
 
 @section('content_header')
-    <h1>Planos</h1>
+<h1>Planos <a href="{{ route('plans.create')}}" class="btn btn-dark">Adicionar</a></h1>
 @stop
 
 @section('content')
-    <p>Listagem dos planos</p>
+<div class="card">
+    <div class="card-header">
+        #filtros
+    </div>
+    <div class="card-body">
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preços</th>
+                    <th width="50px">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($plans as $plan)
+                <tr>
+                    <td>{{ $plan->name }}</td>
+                    <td>{{ $plan->price }}</td>
+                    <td><a href="" class="btn btn-warning">Ver</a></td>
+                </tr>
+
+
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+    <div class="card-footer">
+        {!! $plans->links() !!}
+    </div>
+</div>
+
 @stop
